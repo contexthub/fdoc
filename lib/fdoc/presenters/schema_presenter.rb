a55b76fe63b83e9bcf3b51aa076fb25e08bca4e2
Chris Mason <chris@chaione.com>
2014-12-02 09:15:52 -0600

    use code blocks for examples

--- lib/fdoc/presenters/schema_presenter.rb
+++ lib/fdoc/presenters/schema_presenter.rb
@@ -65,7 +65,7 @@ class Fdoc::SchemaPresenter < Fdoc::BasePresenter
     md << "\n#{prefix}* __Required__: #{required?}" if nested?
     md << "\n#{prefix}* __Type__: #{type}" if type
     md << "\n#{prefix}* __Format__: #{format}" if format
-    md << "\n#{prefix}* __Example__: <tt>#{example.to_markdown}</tt>" if example
+    md << "\n#{prefix}* __Example__: <pre><code>#{example.to_markdown}</code></pre>" if example
     md << "\n#{@schema['enum']}"
     (@schema.keys - Fdoc::SchemaPresenter::FORMATTED_KEYS).each do |key|
       md << "\n#{prefix}* #{key}: #{@schema[key]}"
